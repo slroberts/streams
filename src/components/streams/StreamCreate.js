3c9f5bdbebd9e627384b1553418eea4258cff756
Shomari Roberts <shomariroberts@gmail.com>
2021-07-16 13:09:33 -0400

    crerated form to add new stream with form handling and validation

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,71 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+class StreamCreate extends React.Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className='ui error message'>
+          <div className='header'>{error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = ({ input, label, placeholder, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} placeholder={placeholder} autoComplete='off' />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className='ui form error'
+      >
+        <Field
+          name='title'
+          component={this.renderInput}
+          label='Enter Title'
+          placeholder='ex. Coding Stream'
+        />
+        <Field
+          name='description'
+          component={this.renderInput}
+          label='Enter Description'
+          placeholder='ex. Stream about coding.'
+        />
+        <button className='ui button primary'>Create Stream</button>
+      </form>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = 'You must enter a title!';
+  }
+
+  if (!formValues.description) {
+    errors.description = 'You must enter a description!';
+  }
+
+  return errors;
 };
 
-export default StreamCreate;
+export default reduxForm({
+  form: 'streamCreate',
+  validate,
+})(StreamCreate);
